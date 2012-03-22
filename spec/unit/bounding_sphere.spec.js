7084343729b49a1c81eb4388579b9f47da044047
Unai Orbe <unai@orbe.me>
2012-03-22 13:15:12 +0100

    BoundingSphere and tests added

---
+++ spec/unit/bounding_sphere.spec.js
@@ -0,0 +1,68 @@
+var requirejs = require("requirejs");
+requirejs.config({nodeRequire: require});
+
+requirejs(["goom-math", "../../src/bounding_sphere"], function(Mathematics, BoundingSphere) {
+	describe("Physics.BoundingSphere", function() {
+		beforeEach(function() {
+			this.s = new BoundingSphere();
+		});
+
+		it("should create a bounding sphere containing the two given spheres", function() {
+			var s2 = new BoundingSphere(new Mathematics.Vector3D(1, 0, 0), 1);
+			var s3 = BoundingSphere.createfromSpheres(this.s, s2);
+			expect(s3.radious).toBe(1.5);
+			expect(s3.position.x).toBe(0.5);
+			expect(s3.position.y).toBe(0);
+			expect(s3.position.z).toBe(0);
+
+			s2 = new BoundingSphere(new Mathematics.Vector3D(1, 0, 0), 2);
+			s3 = BoundingSphere.createfromSpheres(this.s, s2);
+			expect(s3.radious).toBe(2);
+			expect(s3.position.x).toBe(1);
+			expect(s3.position.y).toBe(0);
+			expect(s3.position.z).toBe(0);
+
+			this.s.radious = 2;
+			s2 = new BoundingSphere(new Mathematics.Vector3D(1, 0, 0), 1);
+			s3 = BoundingSphere.createfromSpheres(this.s, s2);
+			expect(s3.radious).toBe(2);
+			expect(s3.position.x).toBe(0);
+			expect(s3.position.y).toBe(0);
+			expect(s3.position.z).toBe(0);
+		});
+
+		it('should say the spheres are overlapping when they are', function() {
+			var s2 = new BoundingSphere(new Mathematics.Vector3D(2, 0, 0), 1.1);
+			expect(this.s.overlaps(s2)).toBeTruthy();
+			expect(s2.overlaps(this.s)).toBeTruthy();
+
+			s2 = new BoundingSphere(new Mathematics.Vector3D(0, 2, 0), 1.1);
+			expect(this.s.overlaps(s2)).toBeTruthy();
+			expect(s2.overlaps(this.s)).toBeTruthy();
+
+			s2 = new BoundingSphere(new Mathematics.Vector3D(0, 0, 2), 1.1);
+			expect(this.s.overlaps(s2)).toBeTruthy();
+			expect(s2.overlaps(this.s)).toBeTruthy();
+		});
+
+
+		it('should say the spheres are not overlapping when they are not', function() {
+			var s2 = new BoundingSphere(new Mathematics.Vector3D(2, 0, 0), 1);
+			expect(this.s.overlaps(s2)).toBeFalsy();
+			expect(s2.overlaps(this.s)).toBeFalsy();
+
+			s2 = new BoundingSphere(new Mathematics.Vector3D(0, 2, 0), 1);
+			expect(this.s.overlaps(s2)).toBeFalsy();
+			expect(s2.overlaps(this.s)).toBeFalsy();
+
+			s2 = new BoundingSphere(new Mathematics.Vector3D(0, 0, 2), 1);
+			expect(this.s.overlaps(s2)).toBeFalsy();
+			expect(s2.overlaps(this.s)).toBeFalsy();
+		});
+
+		it("should tell the size the sphere needs to grow to hold the given sphere", function() {
+			var s2 = new BoundingSphere(new Mathematics.Vector3D(1, 0, 0), 1);
+			expect(this.s.growth(s2)).toBe(1.25);
+		});
+	});
+});
---
+++ src/bounding_sphere.js
@@ -0,0 +1,103 @@
+if (typeof define !== 'function') {
+	var define = require('amdefine')(module);
+}
+
+define(["goom-math"], function(Mathematics) {
+	/**
+		Creates a BoundingSphere.
+		@class A BoundingSphere is a kind of Bounding Volume used for broad-phase collision detection in Bounding Volume Hierarchies.
+		@exports BoundingSphere as Physics.BoundingSphere
+		@param {Mathematics.Vector3D} position The origin position of the sphere.
+		@param {Number} [radious=1] The radious of the sphere.
+		@property {Mathematics.Vector3D} position The origin position of the sphere.
+		@property {Number} radious The radious of the sphere.
+		@property {Mathematics.Vector3D} __helperVector This vector is used in a few functions as a local variable,
+			it is defined as a class variable to avoid the creation of objects at runtime.
+	*/
+	var BoundingSphere = (function() {
+		function BoundingSphere(position, radious) {
+			if (position === null || position === undefined) position = new Mathematics.Vector3D();
+			this.radious = radious !== null  && radious !== undefined? radious : 1;
+			this.position = position.clone();
+			this.__helperVector = new Mathematics.Vector3D();
+		}
+
+		/**
+			Creates a BoundingSphere to enclose the two given spheres.
+			@param {Physiscs.BoundingSphere} sphere_one The first bounding sphere to enclose.
+			@param {Physiscs.BoundingSphere} sphere_two The second bounding sphere to enclose.
+			@returns {Physics.BoundingSphere} The constructed BoundingSphere.
+		*/
+		BoundingSphere.createfromSpheres = function(sphere_one, sphere_two) {
+			var center_offset = new Mathematics.Vector3D();
+			sphere_two.position.substract(sphere_one.position, center_offset);
+			var distance = center_offset.squaredMagnitude();
+			var radious_diff = sphere_two.radious - sphere_one.radious;
+
+			if ((radious_diff * radious_diff) >= distance) {
+				if (sphere_one.radious > sphere_two.radious) {
+					position = sphere_one.position.clone();
+					radious = sphere_one.radious;
+				} else {
+					position = sphere_two.position.clone();
+					radious = sphere_two.radious;
+				}
+			} else {
+				distance = Math.sqrt(distance);
+				radious = (distance + sphere_one.radious + sphere_two.radious) * 0.5;
+				position = sphere_one.position.clone();
+				if (distance > 0) position.add(center_offset.scale((radious - sphere_one.radious) / distance));
+			}
+
+			return new BoundingSphere(position, radious);
+		};
+
+		/**
+			Checks wether this sphere and the given sphere are overlapping.
+			@param {Physics.BoundingSphere} sphere The sphere to check overlapping against.
+			@returns {Boolean} True if the spheres are overlapping, false otherwise.
+		*/
+		BoundingSphere.prototype.overlaps = function(sphere) {
+			var position_diff = new Mathematics.Vector3D();
+			var distance_squared = this.position.substract(sphere.position, position_diff).squaredMagnitude();
+			return distance_squared < ((this.radious + sphere.radious) * (this.radious + sphere.radious));
+		};
+
+		/**
+			Returns the volume of this bounding sphere.
+			@returns {Number} The volume of the bounding sphere.
+		*/
+		BoundingSphere.prototype.size = function() {
+			return 1.333333 * Math.PI * this.radious * this.radious * this.radious;
+		};
+
+		/**
+			Returns the growth necessary for the given sphere to fit inside this one.
+			@param {Physics.BoundingSphere} sphere The sphere that must fit inside this one.
+			@returns {Number} The ammount this sphere must be grown to fit the given sphere inside it.
+		*/
+		BoundingSphere.prototype.growth = function(sphere) {
+			var center_offset = sphere.position.substract(this.position, this.__helperVector);
+			var distance = center_offset.squaredMagnitude();
+			var radious_diff = sphere.radious - this.radious;
+			var radious = 0;
+
+			if ((radious_diff * radious_diff) >= distance) {
+				if (this.radious > sphere.radious) {
+					radious = this.radious;
+				} else {
+					radious = sphere.radious;
+				}
+			} else {
+				distance = Math.sqrt(distance);
+				radious = (distance + this.radious + sphere.radious) * 0.5;
+			}
+
+			return radious * radious - this.radious * this.radious;
+		};
+
+		return BoundingSphere;
+	})();
+
+	return BoundingSphere;
+});
